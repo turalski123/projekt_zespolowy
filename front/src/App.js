f3e5f7dfaaaf8fa6ac865076f04da7066c9fca37
Wiktor <gozetero@gmail.com>
2021-03-21 19:33:28 +0100

    Test CD

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadrrr.
+          Edit <code>src/App.js</code> and save to reloadrrrr.
         </p>
         <a
           className="App-link"
