58d8304fed297e55bfd2cc85c9ce9309290fbc2e
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-22 20:39:10 +0100

    added DTO for registration

--- back/src/DTO/RegisterDTO.php
+++ back/src/DTO/RegisterDTO.php
@@ -3,24 +3,51 @@
 namespace App\DTO;
 
 use OpenApi\Annotations as OA;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator as MyAssert;
+use App\Document\User;
 
 class RegisterDTO
 {
     /**
      * @var string
      * @OA\Property(type="string", description="username used to login an User")
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="string"
+     * )
+     * @MyAssert\UniqueField(
+     *     fieldName="username",
+     *     entityClass=User::class
+     * )
      */
     private string $username;
 
     /**
      * @var string
      * @OA\Property(type="string", description="password used to login an User")
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="string"
+     * )
      */
     private string $password;
 
     /**
      * @var string
      * @OA\Property(type="string", description="email of the User")
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="string"
+     * )
+     * @Assert\Email()
+     * @MyAssert\UniqueField(
+     *     fieldName="email",
+     *     entityClass=User::class
+     * )
      */
     private string $email;
 
