20af76f9fc8ac276f470ce9a4698a9e3473e4413
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-22 20:36:33 +0100

    updated dependencies back

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
