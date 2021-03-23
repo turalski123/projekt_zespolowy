8f63f1464ed523bff1defd20522634b646e079d7
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-23 11:08:59 +0100

    fixed example values for queue

--- queue/main.py
+++ queue/main.py
@@ -3,7 +3,8 @@ from celery import Celery
 import urllib.request
 import os
 
-print(os.getenv('ENV_RABBITMQ_DEFAULT_USER'))
-print(os.getenv('ENV_RABBITMQ_DEFAULT_PASS'))
-print(os.getenv('ENV_MONGO_INITDB_ROOT_USERNAME'))
-print(os.getenv('ENV_MONGO_INITDB_ROOT_PASSWORD'))
+print(os.getenv('RABBITMQ_ERLANG_COOKIE'))
+print(os.getenv('RABBITMQ_DEFAULT_USER'))
+print(os.getenv('RABBITMQ_DEFAULT_PASS'))
+print(os.getenv('MONGO_INITDB_ROOT_USERNAME'))
+print(os.getenv('MONGO_INITDB_ROOT_PASSWORD'))
