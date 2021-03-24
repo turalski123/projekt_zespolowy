14673f89e41323425fa75e71108ccd57b3f972ad
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-24 20:47:19 +0100

    modified user document definition

--- back/src/Document/User.php
+++ back/src/Document/User.php
@@ -232,7 +232,10 @@ class User implements UserInterface
      */
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     /**
