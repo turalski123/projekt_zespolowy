045818183dffbcf836f09a5dce8525447053121f
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-25 11:10:24 +0100

    added profiler component

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
