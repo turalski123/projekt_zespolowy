6bcda29551bc314f075a358397b31e25b9e1293f
gh05t-pl <w.zmudzinski01@gmail.com>
2021-03-31 17:10:33 +0200

    updated paginated response

--- back/src/Response/PaginatedApiResponse.php
+++ back/src/Response/PaginatedApiResponse.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PaginatedApiResponse extends JsonResponse
 {
-    public function __construct(int $page, int $count, array $data, int $status = 200, array $headers = [], bool $json = false)
+    public function __construct(int $page, int $limit, int $count, array $data, int $status = 200, array $headers = [], bool $json = false)
     {
         parent::__construct(
             [
                 'success' => $status >= 200 && $status < 300,
                 'data' => [
                     'page' => $page,
+                    'limit' => $limit,
                     'count' => $count,
                     'items' => $data
                 ],
